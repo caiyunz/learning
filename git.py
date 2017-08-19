c736b2e0aea91d4c32d91f095c764acfd5743a57
caiyun792477396 <792477396@qq.com>
2017-08-19 14:31:08 +0800

    master

---
+++ git.py
@@ -0,0 +1,9 @@
+how to using git
+1.在github.com上面注册账号，然后创建一个自己的工程，然后将这个工程的连接记录下来
+2.下载github上面自己的工程
+git clone 工程的连接地址
+3.对工程进行编辑修改
+4.上传修改
+git add .
+git commit -m 'master'
+git push origin master
